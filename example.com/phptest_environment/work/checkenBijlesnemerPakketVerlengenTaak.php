308d6b0ac7b00950f57c2f5edf41c7a5967a3323
Mwalima Peltenburg <mwalima@studentsplus.nl>
2017-01-31 17:30:22 +0100

    checken script

--- example.com/phptest_environment/work/checkenBijlesnemerPakketVerlengenTaak.php
+++ example.com/phptest_environment/work/checkenBijlesnemerPakketVerlengenTaak.php
@@ -4,80 +4,138 @@
 //cron 30 draaien (pakket verlengen) totdat alle cases zijn verlengd
 //pakketten verlengen van alle klanten
 //wat zijn alle pakketen
-**/
+ **/
 class CheckenBijlesnemerPakketVerlengenTaak {
-    
-    
 
-    public function countBijlesnemersPakketVerlengen() {        
-    //selecteer de bijlesnemer op basis van zijn 24687
-    //zoek naar het laatste jaar/maand dat verlengd is (2018-02) uit de planning
-    //zoek naar de pakketten op pakket id (15)
-    //waar de status ongelijk is aan oud(pakket is niet meer actief)
-    //en pakket uren zijn meer dan 0 of als er een pakket stop =`1
-        $query = new Query("SELECT x.bijlesnemer_id as id, p2.jaar, p2.maand from (
-        SELECT  bijlesnemer_id, max(jaar*100+maand) as laatste_periode from dbd_bijles_planning p
-        JOIN dbd_pakketten pak on pak.id = p.pakket_id
-        WHERE p.`status` <> 'oud' and (p.uren > 0 or pak.`stop` = 1 )and (p.jaar > year(now()) or (p.jaar = year(now()) and p.maand >= month(now())))
-        group by bijlesnemer_id
-        ) as x
-        JOIN dbd_bijles_planning p2
-        on p2.bijlesnemer_id = x.bijlesnemer_id and p2.`status` <> 'oud'
-        JOIN dbd_pakketten pak2 on pak2.id = p2.pakket_id
+
+    /**
+     * @return int
+     * selecteer de bijlesnemer op basis van zijn 24687
+     * zoek naar het laatste jaar/maand dat verlengd is (2018-02) uit de planning
+     * zoek naar de pakketten op pakket id (15)
+     * waar de status ongelijk is aan oud(pakket is niet meer actief)
+     * en pakket uren zijn meer dan 0 of als er een pakket stop =`1
+     */
+    public function countBijlesnemersPakketVerlengen() {
+
+        $query = new Query("SELECT
+            x.bijlesnemer_id AS id,
+            p2.jaar,
+            p2.maand
+        FROM
+            (
+                SELECT
+                    bijlesnemer_id,
+                    -- id
+                    max(jaar * 100 + maand) AS laatste_periode -- laatste periode pakket actief --> datum e.q. 201712 (dec 2017)
+                FROM
+                    dbd_bijles_planning p
+                JOIN dbd_pakketten pak ON pak.id = p.pakket_id
+                WHERE
+                    p.`status` <> 'oud' -- waar het pakket ingepland mag worden
+                AND (p.uren > 0 OR pak.`stop` = 1) -- waar je ingeplande uren of een stoppakket hebt
+                AND (
+                    -- en
+                    p.jaar > YEAR (now()) -- het pakket moet in een volgend jaar zijn
+                    OR (
+                        --  of
+                        p.jaar = YEAR (now()) -- in het huidige jaar
+                        AND p.maand >= MONTH (now()) -- maand in de toekomst
+                    )
+                )
+                GROUP BY
+                    bijlesnemer_id
+            ) AS x
+        JOIN dbd_bijles_planning p2 ON p2.bijlesnemer_id = x.bijlesnemer_id -- 
+        AND p2.`status` <> 'oud' -- waar het pakket ingepland mag worden
+        JOIN dbd_pakketten pak2 ON pak2.id = p2.pakket_id
         WHERE
-        (p2.jaar*100+p2.maand) = x.laatste_periode and p2.`status` <> 'oud'
-        and  x.laatste_periode < (year(DATE_ADD(NOW(),	INTERVAL 12 MONTH))*100+ month(DATE_ADD(NOW(),	INTERVAL 12 MONTH)))
-        and pak2.`stop` = 0 AND x.bijlesnemer_id=24276
-        ");
+            (p2.jaar * 100 + p2.maand) = x.laatste_periode -- pak de laatste beschikbare maand uit de planning
+        AND x.laatste_periode < (
+            -- laatste periode is kleiner dan de huidige maand over een jaar (201801)
+            YEAR (
+                DATE_ADD(NOW(), INTERVAL 12 MONTH)
+            ) * 100 + MONTH (NOW())
+        )
+        
+        AND pak2.`stop` = 0 LIMIT 75
+                ");
 
-        return "bijlesplanning regels verlengd : %d\n" . $query->getAffectedRows();
+        return $query->getAffectedRows();
     }
 
-    public function listOfBijlesnemerVerlengenPakket(){
-        //verleng de pakketen
-        $bijlesnemer_id = 24276;
-        $bijlesplanningVerlengen = BijlesPlanningLijst::bijlesnemerPlanningVerlengen($bijlesnemer_id);
-        $periode_over_een_jaar = new YearMonthPeriod(date('Y'), date('m'));
-        $message = "</br>periode die verlengd word ".$periode_over_een_jaar->getYear().$periode_over_een_jaar->getMonth();
-        $message .= "</br>maanden waarmee verlengd word ".$periode_over_een_jaar->addMonthsToString(12);
-        $message .="</br> tot en met ".$periode_over_een_jaar->getYear()." maand: ".$periode_over_een_jaar->getMonth();
-    
-        return $message;
-    }
-
-    public function BijlesnemerPlanningVerlengd() {
-
-        if ($query->getAffectedRows() >= 1) {
-
+    /**
+     * @param int $affectedrows
+     */
+    public function verifyBijlesplanningCronData(int $affectedrows, int $bijlesnemer_id) {
+        $affectedrows = $this->countBijlesnemersPakketVerlengen;
+        $l = new BijlesPlanningLijst();
+        //haal alle bijlesnemers op aan de hand van hun id
+        $l->setFilterBijlesnemer($bijlesnemer_id);
+        $bijlesnemer_id = $l;
+        if ($affectedrows >= 1) {
             $sql = queryPDO("SELECT * FROM  dbd_bijles_planning
-	WHERE
-		bijlesnemer_id = 30379
-	AND `status` <> 'oud'
-	AND uren > 0
-	AND (
-		(
-			YEAR (NOW()) = jaar
-			AND MONTH (NOW()) = maand
-		)
-		OR (
-			YEAR (
-				DATE_ADD(NOW(), INTERVAL 1 YEAR)
-			) = jaar
-			AND MONTH (NOW()) = maand
-		)
-	)");
+        WHERE
+            bijlesnemer_id = '.$bijlesnemer_id.'
+        AND `status` <> 'oud'
+        AND uren > 0
+        AND (
+            (
+                YEAR (NOW()) = jaar
+                AND MONTH (NOW()) = maand
+            )
+            OR (
+                YEAR (
+                    DATE_ADD(NOW(), INTERVAL 1 YEAR)
+                ) = jaar
+                AND MONTH (NOW()) = maand
+            )
+        )");
         }
 
-        foreach ($sql as $row) {
-            echo "<br>";
-            echo "<br>";
-            echo "jaar: " . $row ['jaar'] . "<br>";
-            echo "maand:" . $row['maand'] . "<br>";
-            echo "pakket: " . $row['pakket_id'] . "<br>";
-            echo "uren: " . $row['pakket_uren'] . "<br>";
+        foreach ($sql as $row){
+
+            $message = "bijlesnemer_id".$row['bijlesnemer_id']."<br>";
+            $message .= "jaar: ".$row ['jaar']."<br>";
+            $message .= "maand:".$row['maand']."<br>";
+            $message .= "pakket: ".$row['pakket_id']."<br>";
+            $message = "uren: ".$row['pakket_uren']."<br>";
+
+            $previous = '';
+            if($previous){
+                //
+                if($previous == $row ['jaar']){
+                    echo "pakket niet correct verlengd";
+                }elseif($previous+1 == $row['jaar']){
+                    echo "pakket is correct verlengd";
+                }else{
+                    echo "controleer het pakket voor deze bijlesnemer".$row['bijlesnemer_id'];
+                }
+            }
+            $previous = $row ['jaar'];
+
+        }
+    }
+
+    /**
+     * @param $bijlesnemer_id
+     */
+    public function signalDataIinconsistency($bijlesnemer_id){
+        $laatste_bijles_planning = BijlesPlanningLijst::getLastBijlesPlanning($bijlesnemer_id);
+        if(!$laatste_bijles_planning->isConsistent() && !$laatste_bijles_planning->getBijlesnemer()->isInactiveBijlesnemer()) {
+            $msgSlack = "Pakket van bijlesnemer ".$laatste_bijles_planning->getBijlesnemer()->id." kan niet verlengd worden wegens data inconsistentie: ".$laatste_bijles_planning->getInconsistentReason();
+            SlackWebhook::sendMsg($msgSlack);
+            WarningLijst::addWarning($msgSlack);
+            //return;
         }
     }
 
 }
+
+$newPlanningCheck = new CheckenBijlesnemerPakketVerlengenTaak();
+echo $newPlanningCheck->countBijlesnemersPakketVerlengen();
+
+echo $newPlanningCheck->verifyBijlesplanningCronData($bijlesnemer_id);
+
 
 ?>

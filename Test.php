405bd5ed7970506af526df9e14178e9bdd0fface
Mwalima <m.peltenburg@hotmail.com>
2017-02-12 13:40:27 +0100

    testing travis test 2

--- Test.php
+++ Test.php
@@ -5,6 +5,11 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+        
+        public function testOauth(){
+            $message = "hallo";
+            $this->assertStringStartsWith('prefix', 'foo');
+        }
 }
 
 ?>

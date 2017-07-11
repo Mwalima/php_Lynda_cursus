500d6589d1c09163742e94342678a7ff7921b7f9
mwalima <mwalima@teasemedia.nl>
2017-07-11 14:17:29 +0200

    js en css zit er in

---
+++ example.com/public/js/wisconsinCards.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mwalima on 11-7-17.
+ */
+var WisconsinCardTest = function(){
+
+   alert("hello");
+
+    //initialize
+    var stage = document.getElementById(targetId);
+    var felt = document.createElement("div");
+
+}

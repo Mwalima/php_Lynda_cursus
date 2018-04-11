2e8fc31e602e887910fa9aec98bd3869231e2973
mwalima <m.peltenburg@hotmail.com>
2018-04-11 11:10:55 +0200

    footer logo adjusted

--- example.com/resources/views/partials/footer.blade.php
+++ example.com/resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-lg-12">
-            <p><img src="{{ asset('images/www_logo.png')}}" alt=""></p>
+            <p><img src="{{ asset('images/www_logo_small.png')}}" alt=""></p>
             <p>Made out of pure despair - Copyright 2018</p>
         </div>
     </div><!-- /row -->
